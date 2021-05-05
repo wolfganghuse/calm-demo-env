12fae7c78da5f1a301bc7b6b8e059ba38dd21e08
Wolfgang Huse <wolfgang.huse@nutanix.com>
2021-05-05 16:18:41 +0200

    added dependencies

--- AzureBuildHost/scripts/Package_Package1_Action___install___Task_InstallPackages.sh
+++ AzureBuildHost/scripts/Package_Package1_Action___install___Task_InstallPackages.sh
@@ -3,3 +3,10 @@ sudo yum -y install wget git
 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install epel-release-latest-7.noarch.rpm
+
+sudo wget -P /etc/yum.repos.d/ https://packages.efficios.com/repo.files/EfficiOS-RHEL7-x86-64.repo
+sudo rpmkeys --import https://packages.efficios.com/rhel/repo.key
+
